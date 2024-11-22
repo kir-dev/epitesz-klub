f3fc1d31416ff2569669247ff867d9b00736a2fc
balog-b <bors.balog@gmail.com>
2024-11-22 18:01:05 +0100

    Initial Commit

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,22 @@
+import config from "@payload-config";
+import {getPayload} from "payload";
+
+export default async function Home() {
+
+  const payload = await getPayload({
+    config
+  })
+
+  const data = await payload.find({
+    collection: 'media'
+  })
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Építész Klub</h1>
+      {data.docs.map( (doc, i) => (
+          <img key={i} src={doc.url ? doc.url : ""} alt={doc.alt}></img>
+      ))}
+    </div>
+  );
+}
