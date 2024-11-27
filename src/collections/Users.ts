a78fffac836e119bdc3a82aa8c6d4b5812dbc117
Lipták Péter <liptak.peti.98@gmail.com>
2024-11-27 22:07:13 +0100

    Refactored collection files

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -7,58 +7,7 @@ export const Users: CollectionConfig = {
     },
     auth: true,
     fields: [
-        {
-            name: 'email',
-            label: 'Email',
-            type: 'email',
-            required: true,
-            unique: true,
-        },
-        {
-            name: 'name',
-            label: 'Name',
-            type: 'text',
-            required: true,
-        },
-        {
-            name: 'role',
-            label: 'Role',
-            type: 'select',
-            options: [
-                {
-                    label: 'Elnök',
-                    value: 'elnok',
-                },
-                {
-                    label: 'Általános alelnök',
-                    value: 'altalanos_alelnok',
-                },
-                {
-                    label: 'Külkapcsolati alelnök',
-                    value: 'kulkapcsolati_alelnok',
-                },
-                {
-                    label: 'PR alelnök',
-                    value: 'pr_alelnok',
-                },
-                {
-                    label: 'Gazdasági alelnök',
-                    value: 'gazdasagi_alelnok',
-                },
-                {
-                    label: 'Tag',
-                    value: 'tag',
-                }
-            ],
-            defaultValue: 'tag',
-            required: true,
-        },
-        {
-            name: 'profileImage',
-            label: 'Profile Image',
-            type: 'upload',
-            relationTo: 'media',
-            required: true,
-        }
+        // Email added by default
+        // Add more fields as needed
     ],
 }
