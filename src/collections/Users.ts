9dde763f78b17905d203eaad6a4e373b5dd48aa2
Lipták Péter <liptak.peti.98@gmail.com>
2024-11-27 16:23:35 +0100

    Added user collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,48 @@
-import type { CollectionConfig } from 'payload'
+import type {CollectionConfig} from 'payload'
 
 export const Users: CollectionConfig = {
-  slug: 'users',
-  admin: {
-    useAsTitle: 'email',
-  },
-  auth: true,
-  fields: [
-    // Email added by default
-    // Add more fields as needed
-  ],
+    slug: 'users',
+    admin: {
+        useAsTitle: 'email',
+    },
+    auth: true,
+    fields: [
+        {
+            name: 'email',
+            label: 'Email',
+            type: 'email',
+            required: true,
+            unique: true,
+        },
+        {
+            name: 'name',
+            label: 'Name',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'role',
+            label: 'Role',
+            type: 'select',
+            options: [
+                {
+                    label: 'Admin',
+                    value: 'admin',
+                },
+                {
+                    label: 'User',
+                    value: 'user',
+                },
+            ],
+            defaultValue: 'user',
+            required: true,
+        },
+        {
+            name: 'profileImage',
+            label: 'Profile Image',
+            type: 'upload',
+            relationTo: 'media',
+            required: true,
+        }
+    ],
 }
