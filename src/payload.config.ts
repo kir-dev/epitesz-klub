3a55cb9407c6dd4b84d2007cff24c67c3b2a5980
Danka Marcell <danka.marcell@simonyi.bme.hu>
2024-11-30 22:16:06 +0100

    Implemented the event collection and altered the payload config so that it includes it

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,8 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { Members } from './collections/Members'
+import { Events } from './collections/Events'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,7 +22,7 @@ export default buildConfig({
             baseDir: path.resolve(dirname),
         },
     },
-    collections: [Users, Media, Members],
+    collections: [Users, Media, Members, Events],
     editor: lexicalEditor(),
     secret: process.env.PAYLOAD_SECRET || '',
     typescript: {
