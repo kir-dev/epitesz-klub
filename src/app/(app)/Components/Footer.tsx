7e8f3f86b012ea36972ad0d5b05404e7103d3dd7
Lipták Péter <liptak.peti.98@gmail.com>
2024-12-01 20:09:29 +0100

    Kir-Dev logo added

--- src/app/(app)/Components/Footer.tsx
+++ src/app/(app)/Components/Footer.tsx
@@ -3,6 +3,9 @@
 import React, {useState} from 'react';
 import Link from 'next/link';
 import {Facebook, Instagram, Linkedin, Mail} from 'lucide-react';
+import {TbHeartFilled} from 'react-icons/tb';
+import Image from 'next/image';
+import {LuCopyCheck} from 'react-icons/lu';
 
 const Footer: React.FC = () => {
     const [copied, setCopied] = useState(false);
@@ -14,20 +17,26 @@ const Footer: React.FC = () => {
     };
 
     return (
-        <footer className="w-full py-6 bg-gradient-to-r from-black via-white to-black">
-            <div className="container mx-auto px-4 flex flex-col items-center text-center">
+        <footer className="w-full py-6 bg-gradient-to-r from-black via-white to-black text-black">
+            <div className="container mx-auto px-4 flex flex-col items-center text-center space-y-6">
                 <div className="flex items-center mb-4">
                     <Mail className="h-5 w-5 mr-2 text-gray-800"/>
-                    <button onClick={copyToClipboard} className="text-gray-800 hover:text-gray-600 flex items-center">
-                        <span className="relative">
+                    <button
+                        onClick={copyToClipboard}
+                        className="text-gray-800 hover:text-gray-600 flex items-center"
+                    >
+                        <span className="flex items-center">
                             epiteszklub@uni.bme.hu
-                            <span className="absolute left-full ml-2" style={{whiteSpace: 'pre'}}>
-                                {copied && <span className="text-green-500">{"\t"}Copied!</span>}
-                            </span>
+                            {copied && (
+                                <LuCopyCheck
+                                    className="h-5 w-5 ml-1"
+                                    style={{color: 'rgb(21 128 61)'}}
+                                />
+                            )}
                         </span>
                     </button>
                 </div>
-                <div className="flex space-x-4">
+                <div className="flex space-x-4 mb-4">
                     <Link href="https://www.instagram.com/epitesz_klub_szakkollegium/" target="_blank"
                           rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600">
                         <Instagram className="h-6 w-6"/>
@@ -43,6 +52,13 @@ const Footer: React.FC = () => {
                         <Linkedin className="h-6 w-6"/>
                         <span className="sr-only">LinkedIn</span>
                     </Link>
+                </div>
+                <div className="flex items-center text-gray-800">
+                    Készítette <TbHeartFilled style={{color: 'rgb(239 68 68)'}}/>
+                    -ből a{' '}
+                    <a target='_blank' href='https://kir-dev.hu' rel='noreferrer'>
+                        <Image className='inline' src='/Kir-Dev-vertical.svg' alt='Kir-Dev' width={100} height={20}/>
+                    </a>
                 </div>
             </div>
         </footer>
