f29cabcd606a500b2daed4ec310e7320fabe16cb
Lipták Péter <liptak.peti.98@gmail.com>
2024-12-04 18:38:18 +0100

    Added NavBar and Footer to layout

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,5 @@
 import config from "@payload-config";
 import {getPayload} from "payload";
-import NavBar from "@/app/(app)/Components/navBar";
-import Footer from "@/app/(app)/Components/Footer";
 
 export default async function Home() {
 
@@ -14,16 +12,13 @@ export default async function Home() {
     })
 
     return (
-        <><NavBar/>
+        <>
             <div
                 className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
                 <h1>Építész Klub</h1>
                 {data.docs.map((doc, i) => (
                     <img key={i} src={doc.url ? doc.url : ""} alt={doc.alt}></img>
                 ))}
-            </div>
-            <div>
-                <Footer/>
             </div>
         </>
     );
