bd5dac24a6f67a187b8b3ef7025fdedd2388ccbc
Lipták Péter <liptak.peti.98@gmail.com>
2025-01-24 14:12:38 +0100

    Added page title

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <title>Építész Klub</title>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
