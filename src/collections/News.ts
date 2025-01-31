573b0d757e604ec977b8d90c89f32d3ec94b8d45
Lipták Péter <liptak.peti.98@gmail.com>
2025-01-31 15:02:49 +0100

    Added News collection

---
+++ src/collections/News.ts
@@ -0,0 +1,31 @@
+import type {CollectionConfig} from 'payload'
+
+export const News: CollectionConfig = {
+            slug: 'news',
+            admin: {
+                useAsTitle: 'date',
+                description: 'Ezek a hírek lesznek megjelenítve a főoldalon.',
+            },
+            fields: [
+                {
+                    name: 'date',
+                    label: 'Dátum',
+                    type: 'date',
+                    required: true,
+                },
+                {
+                    name: 'picture',
+                    label: 'Kép',
+                    type: 'upload',
+                    relationTo: 'media',
+                    maxDepth: 1,
+                },
+                {
+                    name: 'description',
+                    label: 'Leírás',
+                    type: 'textarea',
+                    required: true,
+                    maxLength: 150,
+                },
+            ],
+        }
